515df9a0258aa48d09c081e471e30bbdc29aae4e
William-535210013 <william.535210013@stu.untar.ac.id>
2023-06-28 10:45:00 +0700

    hero_dialog_rote update

--- uas/lib/app/navigation/hero_dialog_route.dart
+++ uas/lib/app/navigation/hero_dialog_route.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
 
-/// {@template hero_dialog_route}
-/// Custom [PageRoute] that creates an overlay dialog (popup effect).
-///
-/// Best used with a [Hero] animation.
-/// {@endtemplate}
+
 class HeroDialogRoute<T> extends PageRoute<T> {
-  /// {@macro hero_dialog_route}
   HeroDialogRoute({
     required WidgetBuilder builder,
     RouteSettings? settings,
