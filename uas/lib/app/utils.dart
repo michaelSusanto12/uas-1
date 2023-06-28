8b1f6595b1a6bd7fa7ce649b503d0d018e023b38
William-535210013 <william.535210013@stu.untar.ac.id>
2023-06-28 10:35:29 +0700

    update utils

--- uas/lib/app/utils.dart
+++ uas/lib/app/utils.dart
@@ -4,10 +4,8 @@ import 'package:provider/provider.dart';
 
 import 'state/app_state.dart';
 
-/// Extension method on [BuildContext] to easily perform snackbar operations.
+
 extension Snackbar on BuildContext {
-  /// Removes the current active [SnackBar], and replaces it with a new snackbar
-  /// with content of [message].
   void removeAndShowSnackbar(final String message) {
     ScaffoldMessenger.of(this).removeCurrentSnackBar();
     ScaffoldMessenger.of(this).showSnackBar(
@@ -16,8 +14,7 @@ extension Snackbar on BuildContext {
   }
 }
 
-/// Extension method on [BuildContext] to easily retrieve providers.
+
 extension ProviderX on BuildContext {
-  /// Returns the application [AppState].
   AppState get appState => read<AppState>();
 }
